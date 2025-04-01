40c1a40a8cfa63403c41d1833ddb1095b7218ba3
Ondřej Mičaník <117716222+kulisek24@users.noreply.github.com>
2025-04-01 10:28:40 +0200

    divide_test

--- Pytest/tests/test_calculator_parametrized.py
+++ Pytest/tests/test_calculator_parametrized.py
@@ -77,5 +77,4 @@ def test_divide_by_zero():
     with pytest.raises(ValueError, match="Cannot divide by zero!"):
         calculator.divide(10, 0)
 
-        #gigggzggkuasdsasadasdadsasdaasdsadsadasd
-        
+        #gigggzggkuasdsakokoko
